599cba375f6525408235affd9aafd9fb69f46787
teamsidana-del <teamsidana@gmail.com>
2026-08-16 19:12:29 +0530

    Add CORS support, remove debug logging

--- db.js
+++ db.js
@@ -1,6 +1,5 @@
 const { Pool } = require('pg');
 require('dotenv').config();
-console.log('DB URL being used:', process.env.DATABASE_URL);
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get('/health', (req, res) => {
   res.json({ status: 'ok' });
